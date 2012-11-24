2a954e2ce2aaf9c57a67c4b5d644dac2b8b6192f
Wijnand Warren <god@ansuz.nl>
2012-11-24 22:57:02 +0100

    Adding a Python script that can parse "scripts/npc/items.txt" into the HeroBuildItemList java class. Committing HeroBuildItemList anyway so it can be used out of the box.

---
+++ src/nl/ansuz/android/steam/dota/vo/BuildItemVO.java
@@ -0,0 +1,140 @@
+package nl.ansuz.android.steam.dota.vo;
+
+/**
+ * Representation of a DotA hero build item.
+ * 
+ * @author Wijnand
+ */
+public class BuildItemVO {
+
+	public enum AbilityType {
+		DOTA_ABILITY_TYPE_BASIC, DOTA_ABILITY_TYPE_ULTIMATE, DOTA_ABILITY_TYPE_ATTRIBUTES;
+	}
+
+	public enum AbilityBehavior {
+		/**
+		 * This ability can be owned by a unit but can't be casted and wont show
+		 * up on the HUD.
+		 */
+		DOTA_ABILITY_BEHAVIOR_HIDDEN(1),
+		/**
+		 * Can't be casted like above but this one shows up on the ability HUD.
+		 */
+		DOTA_ABILITY_BEHAVIOR_PASSIVE(2),
+		/**
+		 * Doesn't need a target to be cast, ability fires off as soon as the
+		 * button is pressed.
+		 */
+		DOTA_ABILITY_BEHAVIOR_NO_TARGET(4),
+		/**
+		 * Ability needs a target to be casted on.
+		 */
+		DOTA_ABILITY_BEHAVIOR_UNIT_TARGET(8),
+		/**
+		 * Ability can be cast anywhere the mouse cursor is. (If a unit is
+		 * clicked it will just be cast where the unit was standing)
+		 */
+		DOTA_ABILITY_BEHAVIOR_POINT(16),
+		/**
+		 * This ability draws a radius where the ability will have effect. Kinda
+		 * like POINT but with a an area of effect display.
+		 */
+		DOTA_ABILITY_BEHAVIOR_AOE(32),
+		/**
+		 * This ability probably can be casted or have a casting scheme but
+		 * cannot be learned. (These are usually abilities that are temporary
+		 * like techie's bomb detonate.)
+		 */
+		DOTA_ABILITY_BEHAVIOR_NOT_LEARNABLE(64),
+		/**
+		 * This abillity is channelled. If the user moves or is silenced the
+		 * ability is interrupted.
+		 */
+		DOTA_ABILITY_BEHAVIOR_CHANNELLED(128),
+		/**
+		 * This ability is tied up to an item.
+		 */
+		DOTA_ABILITY_BEHAVIOR_ITEM(256),
+		/**
+		 * This ability can be insta-toggled.
+		 */
+		DOTA_ABILITY_BEHAVIOR_TOGGLE(512),
+		/**
+		 * This ability has a direction from the hero.
+		 */
+		DOTA_ABILITY_BEHAVIOR_DIRECTIONAL(1024),
+		/**
+		 * This ability does not interrupt other abilities.
+		 */
+		DOTA_ABILITY_BEHAVIOR_IMMEDIATE(2048),
+		/**
+		 * No idea what this one is, it isn't documented.
+		 */
+		DOTA_ABILITY_BEHAVIOR_ROOT_DISABLES(4096),
+		/**
+		 * No idea what this one is, it isn't documented.
+		 */
+		DOTA_ABILITY_BEHAVIOR_NOASSIST(8192),
+		/**
+		 * No idea what this one is, it isn't documented.
+		 */
+		DOTA_ABILITY_BEHAVIOR_IGNORE_CHANNEL(16384);
+
+		public final int behaviourValue;
+
+		AbilityBehavior(int value) {
+			behaviourValue = value;
+		}
+	}
+
+	// General
+	/**
+	 * unique ID number for this item.
+	 */
+	public int id;
+	public String abilityName;
+	public int abilityBehavior;
+
+	// Stats
+	public int[] abilityCastRange;
+	public double abilityCastPoint;
+	public double[] abilityCooldown;
+	public int[] abilityManaCost;
+
+	// Item Info
+	public int itemCost;
+	public String[] itemShopTags;
+	public String itemQuality;
+	public boolean sideShop;
+	public String[] invalidHeroes;
+
+	public BuildItemVO(int id, String abilityName, int abilityBehavior,
+			int[] abilityCastRange, double abilityCastPoint,
+			double[] abilityCooldown, int[] abilityManaCost, int itemCost,
+			String[] itemShopTags, String itemQuality, boolean sideShop,
+			String[] invalidHeroes) {
+
+		this.id = id;
+		this.abilityName = abilityName;
+		this.abilityBehavior = abilityBehavior;
+
+		this.abilityCastRange = abilityCastRange;
+		this.abilityCastPoint = abilityCastPoint;
+		this.abilityCooldown = abilityCooldown;
+		this.abilityManaCost = abilityManaCost;
+
+		this.itemCost = itemCost;
+		this.itemShopTags = itemShopTags;
+		this.itemQuality = itemQuality;
+		this.sideShop = sideShop;
+		this.invalidHeroes = invalidHeroes;
+	}
+
+	/**
+	 * Creates a URL to download the item image from.
+	 */
+	public String getImageUrl() {
+		return "http://media.steampowered.com/apps/dota2/images/items/"
+				+ abilityName.replace("item_", "") + "_lg.png";
+	}
+}
---
+++ tools/itemparser/BuildItemsParser.py
@@ -0,0 +1,199 @@
+import ConfigParser
+import os
+import re
+
+class BuildItemsParser:
+    
+    # Config
+    dotaBuildItemsFile = ""
+    javaTemplateLocation = ""
+    javaTargetLocation = ""
+    itemsToken = ""
+    itemCountToken = ""
+    
+    # Item parsing
+    itemContents = ""
+    javaTemplate = ""
+    javaOutput = ""
+    compiledOpenPattern = None
+    compiledClosePattern = None
+    
+    def __init__(self):
+        print "===================================="
+        print "=   DotA hero build items parser   ="
+        print "===================================="
+        
+        self.compiledOpenPattern = re.compile("^\t{$", re.DOTALL | re.MULTILINE)
+        self.compiledClosePattern = re.compile("^\t}$", re.DOTALL | re.MULTILINE)
+        
+        self.loadConfig();
+        self.loadItemsFile();
+        self.loadJavaTemplate();
+        self.parseAllItems();
+        self.saveJavaFile();
+
+    """
+        Loads and parses the config file for the item parser. 
+    """
+    def loadConfig(self):
+        print "loadConfig():"
+        
+        config = ConfigParser.ConfigParser()
+        file = open("BuildItemsParser.cfg")
+        config.readfp(file)
+        file.close()
+        
+        self.dotaBuildItemsFile = config.get("Items", "dotaBuildItemsFile")
+        self.javaTemplateLocation = config.get("Java", "template")
+        self.javaTargetLocation = config.get("Java", "target")
+        self.itemsToken = config.get("Java", "itemsToken")
+        self.itemCountToken = config.get("Java", "itemCountToken")
+        
+        print " - Items file: " + self.dotaBuildItemsFile
+        print " - Java template: " + self.javaTemplateLocation
+        print " - Java target: " + self.javaTargetLocation
+        print " - Java items token: " + self.itemsToken
+        print " - Java item count token: " + self.itemCountToken
+
+    """
+        Loads the DotA items file.
+    """
+    def loadItemsFile(self):
+        print "loadItemsFile(): " + self.dotaBuildItemsFile
+        file = open(self.dotaBuildItemsFile)
+        self.itemContents = file.read()
+        file.close()
+        
+    """
+        Loads the Java template.
+    """
+    def loadJavaTemplate(self):
+        print "loadJavaTemplate(): " + self.javaTemplateLocation
+        file = open(self.javaTemplateLocation)
+        self.javaTemplate = file.read()
+        file.close()
+    
+    """
+        Parses the DotA items file.
+    """
+    def parseAllItems(self):
+        print "parseAllItems()"
+        # store in self.javaOutput
+        itemPosition = self.getItemPosition(0)
+        
+        while itemPosition:
+            #print " - item: %s" % (itemPosition,)
+            itemString = self.itemContents[itemPosition[0]:itemPosition[1]]
+            # Grab attrs
+            self.javaOutput += "\t\t" + self.parseSingleItem(itemString) + "\n"
+            itemPosition = self.getItemPosition(itemPosition[1])
+            
+    """
+        Finds the start and end position of an item to parse.
+        
+        @param startPosition: position to start searching from.
+    """
+    def getItemPosition(self, startPosition):
+        result = None
+        
+        match = self.compiledOpenPattern.search(self.itemContents, startPosition)
+        if match:
+            itemStart = match.start()
+            match = self.compiledClosePattern.search(self.itemContents, itemStart)
+            itemEnd = match.start()
+            result = itemStart, itemEnd
+        
+        return result
+        
+    """
+        Parses a single hero item and returns the Java string that we need to
+        inject.
+        
+        @param item: The item to parse.
+    """
+    def parseSingleItem(self, item):
+        id = self.findPropertyValue(item, "ID", "-1")
+        abilityName = self.findPropertyValue(item, "AbilityName", "")
+        abilityBehavior = self.findPropertyValue(item, "AbilityBehavior", "-1")
+        
+        abilityCastRange = self.findPropertyValue(item, "AbilityCastRange", "")
+        abilityCastPoint = self.findPropertyValue(item, "AbilityCastPoint", "-1")
+        abilityCooldown = self.findPropertyValue(item, "AbilityCooldown", "")
+        abilityManaCost = self.findPropertyValue(item, "AbilityManaCost", "")
+        
+        itemCost = self.findPropertyValue(item, "ItemCost", "-1")
+        itemShopTags = self.findPropertyValue(item, "ItemShopTags", "")
+        itemQuality = self.findPropertyValue(item, "ItemQuality", "")
+        sideShop = self.findPropertyValue(item, "SideShop", "0")
+        invalidHeroes = self.findPropertyValue(item, "InvalidHeroes", "")
+        
+        result = "itemList[" + id + "] = new BuildItemVO("
+        result += id + ", "
+        result += "\"" + abilityName + "\", "
+        result += "-1, " if abilityBehavior == "-1" else "BuildItemVO.AbilityBehavior." + abilityBehavior.replace(" | ", ".behaviourValue + BuildItemVO.AbilityBehavior.") + ".behaviourValue, "
+        
+        result += "null, " if abilityCastRange == "" else "new int[]{" + abilityCastRange.replace(" ", ", ") + "}, "
+        result += abilityCastPoint + ", "
+        result += "null, " if abilityCooldown == "" else "new double[]{" + abilityCooldown.replace(" ", ", ") + "}, "
+        result += "null, " if abilityManaCost == "" else "new int[]{" + abilityManaCost.replace(" ", ", ") + "}, "
+        
+        result += itemCost + ", "
+        result += "null, " if itemShopTags == "" else "new String[]{\"" + itemShopTags.replace(";", "\", \"") + "\"}, "
+        result += "\"" + itemQuality + "\", "
+        result += ("true" if sideShop == "1" else "false")  + ", "
+        result += "null" if invalidHeroes == "" else "new String[]{\"" + invalidHeroes.replace(";", "\", \"") + "\"}"
+        result += ");"
+        
+        return result
+        
+    """
+        Finds a single property's value.
+        
+        @param item: The item to search through.
+        @param propertyName: The property we're looking for.
+        @param defaultValue: Default value to use if the property (or value) 
+            can't be found. 
+    """
+    def findPropertyValue(self, item, propertyName, defaultValue):
+        result = defaultValue;
+        namePosition = item.find(propertyName)
+        
+        if namePosition > -1:
+            valueStart = item.find("\"", namePosition + len(propertyName) + 1)
+            if valueStart > -1:
+                valueStart += 1
+                valueEnd = item.find("\"", valueStart)
+                if valueEnd > -1:
+                    result = item[valueStart:valueEnd]
+        
+        return result
+        
+    """
+        Finds the field that indicates how many IDs are already in use.
+    """
+    def getItemCount(self):
+        print "getItemCount()"
+        countIdentifier = "// next free ID: "
+        
+        countStart = self.itemContents.find(countIdentifier)
+        countStart += len(countIdentifier)
+        
+        countEnd = self.itemContents.find("\n", countStart)
+        
+        return self.itemContents[countStart:countEnd]
+        
+    """
+        Save the Java class.
+    """
+    def saveJavaFile(self):
+        print "saveJavaFile(): " + self.javaTargetLocation
+        fileOutput = self.javaTemplate.replace(self.itemsToken, self.javaOutput)
+        fileOutput = fileOutput.replace(self.itemCountToken, self.getItemCount())
+        
+        file = open(self.javaTargetLocation, "w")
+        file.write(fileOutput);
+        file.close();
+        print "Done!"
+
+if __name__ == "__main__":
+    BuildItemsParser()
