eb8ad55868c9b817a607cc0da27a317e36ba210e
Wijnand Warren <god@ansuz.nl>
2012-12-22 15:01:08 +0100

    Updating GsonUtil to strip both Steam and DotA response element from JSON.

--- src/nl/ansuz/android/steam/util/GsonUtil.java
+++ src/nl/ansuz/android/steam/util/GsonUtil.java
@@ -11,18 +11,42 @@ import com.google.gson.JsonElement;
 public class GsonUtil {
 
 	/**
-	 * Strips the "result" Object that surrounds all Steam API JSON responses.
+	 * Strips the "result" Object that surrounds all Dota Steam API JSON 
+	 * responses.
 	 * 
 	 * @param json The Steam JSON response to strip.
-	 * @return A JsonElement that can be used to deserialize the Steam API response.
+	 * @return A JsonElement that can be used to deserialize the Steam API 
+	 * 	response.
 	 */
-	public JsonElement getStrippedResult(String json) {
+	protected JsonElement getStrippedResult(String json, String elementName) {
 		Gson gson = new Gson();	
 		JsonElement rawMatchHistoryResponse = gson.fromJson(json, JsonElement.class);
-		JsonElement result = rawMatchHistoryResponse.getAsJsonObject().get("result");
+		JsonElement result = rawMatchHistoryResponse.getAsJsonObject().get(elementName);
 		gson = null;
 		
 		return result;
+	}
+	
+	/**
+	 * Strips the "result" Object that surrounds all Dota Steam API JSON 
+	 * responses.
+	 * 
+	 * @see getStrippedResult(String json, String elementName).
+	 * @param json The Steam JSON response to strip.
+	 */
+	public JsonElement getStrippedDotaResult(String json) {
+		return getStrippedResult(json, "result");
+	}
+	
+	/**
+	 * Strips the "response" Object that surrounds all generic Steam API JSON 
+	 * responses.
+	 * 
+	 * @see getStrippedResult(String json, String elementName).
+	 * @param json The Steam JSON response to strip.
+	 */
+	public JsonElement getStrippedSteamResult(String json) {
+		return getStrippedResult(json, "response");
 	}
 	
 }
