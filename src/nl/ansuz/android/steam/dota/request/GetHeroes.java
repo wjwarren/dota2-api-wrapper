3921a8533aab7442658804c802ab4cd44b1579ab
Wijnand Warren <god@ansuz.nl>
2013-06-26 21:57:26 +0200

    Adding new request parameter.

--- src/nl/ansuz/android/steam/dota/request/GetHeroes.java
+++ src/nl/ansuz/android/steam/dota/request/GetHeroes.java
@@ -8,30 +8,38 @@ import nl.ansuz.android.steam.request.ISteamRequest;
  * @author Wijnand
  */
 public class GetHeroes extends DotaRequest implements ISteamRequest {
-	
+
 	/**
 	 * Simple listing of available request parameters.
 	 */
 	public class Parameter {
+		/**
+		 * (Optional) (String) The ISO639-1 language code for the language to provide hero names in.
+		 */
 		public final static String LANGUAGE = "language";
+
+		/**
+		 *(Optional) (boolean) Return a list of itemized heroes only.
+		 */
+		public final static String ITEMIZED_ONLY = "itemizedonly";
 	}
-	
+
 	private final static String METHOD_NAME = "GetHeroes";
 	private final static String INTERFACE_PREFIX = "IEconDOTA2_";
-	
+
 	@Override
 	protected String getInterface(int applicationId) {
 		return INTERFACE_PREFIX + applicationId;
 	}
-	
+
 	@Override
 	protected String getBaseUrl(int appId) {
 		String baseUrl = super.getBaseUrl(appId);
 		baseUrl += "/" + METHOD_NAME;
 		baseUrl += "/" + API_VERSION_ONE;
 		baseUrl += "/";
-		
+
 		return baseUrl;
 	}
-	
+
 }
