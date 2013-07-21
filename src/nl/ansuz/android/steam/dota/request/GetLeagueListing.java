d7866cca3d9eb5e0a5aa9e0208b0ee8f1050328a
Wijnand Warren <god@ansuz.nl>
2013-07-21 10:59:47 +0200

    Some code clean up.

--- src/nl/ansuz/android/steam/dota/request/GetLeagueListing.java
+++ src/nl/ansuz/android/steam/dota/request/GetLeagueListing.java
@@ -14,26 +14,27 @@ public class GetLeagueListing extends DotaRequest implements ISteamRequest {
 	 * Simple listing of available request parameters.
 	 */
 	public class Parameter {
-		
+
 		/**
 		 * The ISO639-1 language code for the language all tokenized strings should be returned in.
-		 * 
+		 *
 		 * This is an optional parameter. If you omit it, all you get back in the name and description fields are the
 		 * tokens used to look up the translated strings. An example: "name": "#DOTA_Item_Killing_Spree_North_America".
 		 */
 		public final static String LANGUAGE = "language";
-	
+
 	}
-	
+
 	public final static String METHOD_NAME = "GetLeagueListing";
-	
+
+	/** {@inheritDoc} */
 	@Override
 	protected String getBaseUrl(int appId) {
 		String baseUrl = super.getBaseUrl(appId);
 		baseUrl += "/" + METHOD_NAME;
 		baseUrl += "/" + API_VERSION_ONE;
 		baseUrl += "/";
-		
+
 		return baseUrl;
 	}
 }
--- src/nl/ansuz/android/steam/dota/request/GetTeamInfoByTeamId.java
+++ src/nl/ansuz/android/steam/dota/request/GetTeamInfoByTeamId.java
@@ -14,28 +14,29 @@ public class GetTeamInfoByTeamId extends DotaRequest implements ISteamRequest {
 	 * Simple listing of available request parameters.
 	 */
 	public class Parameter {
-		
+
 		/**
 		 * The team id to start returning results from.
 		 */
 		public final static String START_AT_TEAM_ID = "start_at_team_id";
-		
+
 		/**
 		 * The amount of teams to return.
 		 */
 		public final static String NUMBER_OF_TEAMS_REQUESTED = "teams_requested";
 	}
-	
+
 	public final static String METHOD_NAME = "GetTeamInfoByTeamID";
-	
+
+	/** {@inheritDoc} */
 	@Override
 	protected String getBaseUrl(int appId) {
 		String baseUrl = super.getBaseUrl(appId);
 		baseUrl += "/" + METHOD_NAME;
 		baseUrl += "/" + API_VERSION_ONE;
 		baseUrl += "/";
-		
+
 		return baseUrl;
 	}
-	
+
 }
