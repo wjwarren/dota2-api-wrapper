bc5cac731317b5ccc6b316dcfc85b9054b51383e
Wijnand Warren <god@ansuz.nl>
2013-09-03 22:22:13 +0200

    Adding new hero image sizes.

--- src/nl/ansuz/android/steam/dota/vo/HeroVO.java
+++ src/nl/ansuz/android/steam/dota/vo/HeroVO.java
@@ -10,8 +10,12 @@ import com.google.gson.annotations.SerializedName;
 public class HeroVO {
 
 	public static final String IMAGE_URL_PREFIX = "http://media.steampowered.com/apps/dota2/images/heroes/";
+
 	public static final String FULL_IMAGE_URL_SUFFIX = "_full.png";
+	public static final String LARGE_IMAGE_URL_SUFFIX = "_lg.png";
 	public static final String SMALL_IMAGE_URL_SUFFIX = "_sb.png";
+	public static final String PORTRAIT_IMAGE_URL_SUFFIX = "_vert.jpg";
+
 	public static final String HERO_NAME_PREFIX = "npc_dota_hero_";
 	public static final String HERO_NAME_PREFIX_REPLACEMENT = "";
 
@@ -35,16 +39,30 @@ public class HeroVO {
 	}
 
 	/**
-	 * @return The URL for the full sized Hero icon.
+	 * @return The URL for the full sized Hero icon (256x144 px).
 	 */
 	public String getFullImageUrl() {
 		return getImageUrl(FULL_IMAGE_URL_SUFFIX);
 	}
 
 	/**
-	 * @return The URL for the small Hero icon.
+	 * @return The URL for the large Hero icon (205x115 px).
+	 */
+	public String getLargeImageUrl() {
+		return getImageUrl(LARGE_IMAGE_URL_SUFFIX);
+	}
+
+	/**
+	 * @return The URL for the small Hero icon (59x33 px).
 	 */
 	public String getSmallImageUrl() {
 		return getImageUrl(SMALL_IMAGE_URL_SUFFIX);
+	}
+	
+	/**
+	 * @return The URL for the portait Hero icon (235x272 px).
+	 */
+	public String getPortaitImageUrl() {
+		return getImageUrl(PORTRAIT_IMAGE_URL_SUFFIX);
 	}
 }
